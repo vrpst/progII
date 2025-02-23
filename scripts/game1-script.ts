8944960fd8555983e3058bf5f28bc17b5991d8f4
Harrison Baghurst <070806harrison@gmail.com>
2025-02-23 15:25:09 +0000

    added scrolling notes

--- scripts/game1-script.ts
+++ scripts/game1-script.ts
@@ -1,17 +1,19 @@
-var notelist:number[] = new Array(13); 
+let numNotes:number = 50;
+let notelist:number[] = new Array(numNotes); 
 for (let i = 0; i < notelist.length; i++) {
     notelist[i] = 1;
     notelist[i] = Math.floor(Math.random() * 3); 
 }
 
-let svgCode = `<svg width="60rem" height="15rem" viewBox="0 0 60 15" xmlns="http://www.w3.org/2000/svg">`;
+let svgWidth = (numNotes + 1) * 5
+let svgCode = `<svg width="${svgWidth.toString()}rem" height="13rem" viewBox="0 0 ${svgWidth.toString()} 13" xmlns="http://www.w3.org/2000/svg">`;
 let currentX:number = 5; 
-for (let i = 0; i < 11; i++) {
-    svgCode = svgCode + `<line class="thin-line" x1="${currentX}" y1="0" x2="${currentX}" y2="15" stroke-linecap="round"/>`;
+for (let i = 0; i < numNotes; i++) {
+    svgCode = svgCode + `<line class="thin-line" x1="${currentX}" y1="0" x2="${currentX}" y2="13" stroke-linecap="round"/>`;
     currentX += 5;
 }
 svgCode = svgCode + `<path id="noteslist" class="line" d="`;
-let prevX:number = 0;
+let prevX:number = 5;
 let prevY:number = 0;
 let prevHeight:number|null = null; 
 for (let i = 0; i < notelist.length; i++) {
@@ -22,12 +24,12 @@ for (let i = 0; i < notelist.length; i++) {
             prevY = 2.5; 
         }
         else if (notelist[i] == 1) {
-            svgCode = svgCode + `M ${prevX.toString()} 7.5 `
-            prevY = 7.5;
+            svgCode = svgCode + `M ${prevX.toString()} 6.5 `
+            prevY = 6.5;
         }
         else if (notelist[i] == 2) {
-            svgCode = svgCode + `M ${prevX.toString()} 12.5 `
-            prevY = 12.5;
+            svgCode = svgCode + `M ${prevX.toString()} 10.5 `
+            prevY = 10.5;
         }
     }
     else {
@@ -35,38 +37,39 @@ for (let i = 0; i < notelist.length; i++) {
             svgCode = svgCode + `L ${(prevX+5).toString()} ${prevY.toString()} `
         }   
         else if (notelist[i] == prevHeight + 1) {
-            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY+2.5).toString()} Q ${(prevX+2.5).toString()} ${(prevY+5).toString()} ${(prevX+5).toString()} ${(prevY+5).toString()} `;
-            prevY += 5;
+            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY+2).toString()} Q ${(prevX+2.5).toString()} ${(prevY+4).toString()} ${(prevX+5).toString()} ${(prevY+4).toString()} `;
+            prevY += 4;
             prevHeight = notelist[i];
         }
         else if (notelist[i] == prevHeight - 1) {
-            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY-2.5).toString()} Q ${(prevX+2.5).toString()} ${(prevY-5).toString()} ${(prevX+5).toString()} ${(prevY-5).toString()} `;
-            prevY -= 5;
+            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY-2).toString()} Q ${(prevX+2.5).toString()} ${(prevY-4).toString()} ${(prevX+5).toString()} ${(prevY-4).toString()} `;
+            prevY -= 4;
             prevHeight = notelist[i];
         }
         else if (notelist[i] == prevHeight + 2) {
-            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY+2.5).toString()} L ${(prevX+2.5)} ${(prevY+7.5)} Q ${(prevX+2.5).toString()} ${(prevY+10).toString()} ${(prevX+5).toString()} ${(prevY+10).toString()} `;
-            prevY += 10;
+            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY+2).toString()} L ${(prevX+2.5)} ${(prevY+6)} Q ${(prevX+2.5).toString()} ${(prevY+8).toString()} ${(prevX+5).toString()} ${(prevY+8).toString()} `;
+            prevY += 8;
             prevHeight = notelist[i];
         }
         else if (notelist[i] == prevHeight - 2) {
-            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY-2.5).toString()} L ${(prevX+2.5)} ${(prevY-7.5)} Q ${(prevX+2.5).toString()} ${(prevY-10).toString()} ${(prevX+5).toString()} ${(prevY-10).toString()} `;
-            prevY -= 10;
+            svgCode = svgCode + `Q ${(prevX+2.5).toString()} ${prevY.toString()} ${(prevX+2.5).toString()} ${(prevY-2).toString()} L ${(prevX+2.5)} ${(prevY-6)} Q ${(prevX+2.5).toString()} ${(prevY-8).toString()} ${(prevX+5).toString()} ${(prevY-8).toString()} `;
+            prevY -= 8;
             prevHeight = notelist[i];
         }
         prevX += 5; 
     }
 }
 svgCode = svgCode + `" stroke-linecap="round"/></svg>`;
-//svgCode = svgCode + `<path id="noteslist" class="line" d="M 0 7.5 L 5 7.5 Q 7.5 7.5 7.5 5 Q 7.5 2.5 10 2.5 Q 12.5 2.5 12.5 5 L 12.5 10 Q 12.5 12.5 15 12.5 L 20 12.5" stroke-linecap="round"/></svg>`;
 let svgContainer = document.createElement('div'); 
-svgContainer.id = 'game-container';
+svgContainer.id = 'svg-container';
 svgContainer.innerHTML = svgCode;
-document.body.appendChild(svgContainer);
-
-let root = document.querySelector(':root') as HTMLElement | null; 
-let path = document.querySelector('#noteslist') as SVGGeometryElement | null;
-if (root && path) {
-    let pathLength = path.getTotalLength(); 
-    root.style.setProperty('--notes-path-length', pathLength.toString());
+let gameContainer = document.getElementById('game-inner') as HTMLElement | null;
+if (gameContainer) {
+    gameContainer.appendChild(svgContainer);
 }
+svgContainer.style.transitionDuration = `${(numNotes * 0.5 + 5).toString()}s` // note separation time + travel time to cross the screen
+svgContainer.style.left =  `-${svgWidth.toString()}rem`
+setTimeout(function(){
+    svgContainer.style.left = `58rem`;
+},1);
+
