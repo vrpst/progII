b9dec70e8c2ec05782c5ec3294fbf21fa90a912d
Thomas Blair <tomblair3008@yahoo.com>
2025-02-24 17:54:57 +0000

    Added static and dynamic circles to the wave :)

--- scripts/game1-script.ts
+++ scripts/game1-script.ts
@@ -5,7 +5,6 @@ for (let i = 0; i < notelist.length; i++) {
     notelist[i] = 1;
     notelist[i] = Math.floor(Math.random() * 3); 
 }
-
 // create svg of background notes timeings 
 let svgWidth = (numNotes + 1) * 5
 let svgCode = `<svg width="${svgWidth.toString()}rem" height="13rem" viewBox="0 0 ${svgWidth.toString()} 13" xmlns="http://www.w3.org/2000/svg">`;
@@ -14,7 +13,6 @@ for (let i = 0; i < numNotes; i++) {
     svgCode = svgCode + `<line class="thin-line" x1="${currentX}" y1="0" x2="${currentX}" y2="13" stroke-linecap="round"/>`;
     currentX += 5;
 }
-
 // create svg connecting notes 
 svgCode = svgCode + `<path id="noteslist" class="line" d="`;
 let prevX:number = 5;
@@ -63,12 +61,26 @@ for (let i = 0; i < notelist.length; i++) {
         prevX += 5; 
     }
 }
-svgCode = svgCode + `" stroke-linecap="round"/></svg>`;
-
+svgCode = svgCode + `" stroke-linecap="round"/>`;
+for (let i = 0, currentX = 5; i < notelist.length; i++, currentX += 5) {
+    if (notelist[i] == 0) {
+        svgCode = svgCode + `<circle class="circle" cx="${currentX}" cy = "2.5" r="0.6" fill="var(--red)"/>`
+    }
+    else if (notelist[i] == 1) {
+        svgCode = svgCode + `<circle class="circle" cx="${currentX}" cy = "6.5" r="0.6" fill="var(--red)"/>`
+    }
+    else {
+        svgCode = svgCode + `<circle class="circle" cx="${currentX}" cy = "10.5" r="0.6" fill="var(--red)"/>`
+    }
+}
+svgCode = svgCode + `</svg>`
 // create static svg 
 let staticSvgCode = `
 <svg width="58rem" height="13rem" viewBox="0 0 58 13" xmlns="http://www.w3.org/2000/svg">
     <line class="thick-line" x1="50" y1="0.75" x2="50" y2="12.25" stroke-linecap="round"/>
+    <circle class="static-circle" cy = "2.5" fill="var(--grey)"/>
+    <circle class="static-circle" cy = "6.5" fill="var(--grey)"/>
+    <circle class="static-circle" cy = "10.5" fill="var(--grey)"/>
 </svg>`
 
 // add svgs to "game-inner" div
