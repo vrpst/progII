33399ebdd9b06940b991a56ca5c153367244c9ad
James Wallwork <jamesleewallwork@gmail.com>
2025-03-05 13:58:28 +0000

    fix dist location

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -19,6 +19,7 @@ module.exports = {
     },
     output: {
         filename: "[name].js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "client/dist"),
     },
-mode: 'development'};
+    mode: "development",
+};
