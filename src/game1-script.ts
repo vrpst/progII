452b3541daf355ed6fe76dc516a6790f3dd30303
Harrison Baghurst <070806harrison@gmail.com>
2025-03-05 16:15:13 +0000

    added basic class setup

--- src/game1-script.ts
+++ src/game1-script.ts
@@ -19,6 +19,60 @@ if (gameContainer) {
     gameContainer.appendChild(canvas);
 }
 
+// level class 
+class Level {
+    private _speed: number | null = null; 
+    private _level: {[key: string]: number}[][] | null = null; 
+    private _phrases: Phrase[] | null = null;
+
+    constructor(speed: number | null, level: {[key: string]: number}[][] | null) {
+        if (speed) { 
+            this._speed = speed;
+        }
+        if (level) {
+            this._level = level; 
+            this.createPhrases();
+        }
+    }
+
+    createPhrases() {
+        for (let phrase of this._level) {
+            this._phrases.push(new Phrase(phrase));
+        }
+    }
+}
+
+// phrase class 
+class Phrase {
+    private _notes: Note[] | null = null;
+    private _phrase: {[key: string]: number}[] | null = null;
+
+    constructor(phrase: {[key: string]: number}[] | null) { 
+        if (phrase) { 
+            this._phrase = phrase;
+            this.createNotes();
+        }
+    }
+
+    createNotes() {
+        for (let note of this._phrase) { 
+            this._notes.push(new Note(note));
+        }
+    }
+}
+
+// note class 
+class Note {
+    private _note: {[key: string]: number} | null = null;
+
+    constructor(note: {[key: string]: number} | null){ 
+        if (note) {
+            this._note = note;
+        }
+    }
+}
+
+/*
 // draw on canvas 
 if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
@@ -28,7 +82,7 @@ if (canvas.getContext) {
     else { 
         ctx.strokeStyle = 'red';
     }
-    ctx.lineWidth = remToPixels(0.5);
+    ctx.lineWidth = remToPixels(1);
     ctx.beginPath();
     ctx.arc(remToPixels(6.5), canvas.height/2, remToPixels(0.5), 0, 2*Math.PI);
     ctx.stroke();
@@ -40,3 +94,4 @@ else {
     // make this output something to the user later e.g. unsupported browser
     console.log('canvas not supported on this browser');
 }
+*/
