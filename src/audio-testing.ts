260090b0289a984b789697fc5f66cf28dcf9cfb9
vrpst <lucas.verpoest@gmail.com>
2025-03-05 12:20:14 +0000

    start webaudio

---
+++ src/audio-testing.ts
@@ -0,0 +1,11 @@
+// start audio
+const audio_context = new AudioContext()
+const audio_element = document.querySelector('audio')
+let audio = audio_context.createMediaElementSource(audio_element)
+audio.connect(audio_context.destination)
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    audio_element.play()
+    console.log("PLAYING")
+})
