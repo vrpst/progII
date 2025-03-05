7ab2e02836bbae4942dc05ef612dda904af71913
James Wallwork <jamesleewallwork@gmail.com>
2025-03-05 13:44:13 +0000

    add npm start

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,10 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 
+app.use(express.static("client"));
 
-app.use(express.static('client'));
-
-console.log('https://localhost:8080');
+console.log("https://localhost:8080");
 
 app.listen(8080);
+
