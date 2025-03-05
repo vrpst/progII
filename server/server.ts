9130ecd050481216c950b4bf37e9b0c86a4a770c
James Wallwork <jamesleewallwork@gmail.com>
2025-03-05 14:13:03 +0000

    serve index

--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 
 app.use(express.static("client"));
+app.use(express.static("client/pages"));
 
 console.log("https://localhost:8080");
 
 app.listen(8080);
-
