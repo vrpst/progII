5468383c073b00f8215400acb75f479db22fc117
Harrison Baghurst <070806harrison@gmail.com>
2025-03-06 00:05:31 +0000

    added foundations of drawing

--- src/game1-script.ts
+++ src/game1-script.ts
@@ -2,6 +2,65 @@ function remToPixels(rem:number) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
+// level class 
+class Level {
+    private _speed: number | null = null; 
+    private _level: {[key: string]: number}[][] | null = null; 
+    private _phrases: Phrase[] = [];
+
+    constructor(speed: number, level: {[key: string]: number}[][]) {
+        this._speed = speed;
+        this._level = level; 
+        this.createPhrases();
+    }
+
+    createPhrases() {
+        for (let phrase of this._level) {
+            this._phrases.push(new Phrase(phrase));
+        }
+    }
+
+    drawNotes(phraseNo: number) { 
+        this._phrases[phraseNo].drawNotes();
+    }
+}
+
+// phrase class 
+class Phrase {
+    private _notes: Note[] = [];
+    private _phrase: {[key: string]: number}[] | null = null;
+
+    constructor(phrase: {[key: string]: number}[]) { 
+        this._phrase = phrase;
+        this.createNotes();
+    }
+
+    createNotes() {
+        for (let note of this._phrase) { 
+            this._notes.push(new Note(note));
+        }
+    }
+
+    drawNotes() {
+        for (let note of this._notes) {
+            note.draw();
+        }
+    }
+}
+
+// note class 
+class Note {
+    private _note: {[key: string]: number} | null = null;
+
+    constructor(note: {[key: string]: number}){ 
+        this._note = note;
+    }
+
+    draw() { 
+        console.log(this._note); 
+    }
+}
+
 // get css root variables 
 let rootStyle = window.getComputedStyle(document.body) as CSSStyleDeclaration | null; 
 let red = null;
@@ -19,58 +78,12 @@ if (gameContainer) {
     gameContainer.appendChild(canvas);
 }
 
-// level class 
-class Level {
-    private _speed: number | null = null; 
-    private _level: {[key: string]: number}[][] | null = null; 
-    private _phrases: Phrase[] | null = null;
-
-    constructor(speed: number | null, level: {[key: string]: number}[][] | null) {
-        if (speed) { 
-            this._speed = speed;
-        }
-        if (level) {
-            this._level = level; 
-            this.createPhrases();
-        }
-    }
-
-    createPhrases() {
-        for (let phrase of this._level) {
-            this._phrases.push(new Phrase(phrase));
-        }
-    }
-}
-
-// phrase class 
-class Phrase {
-    private _notes: Note[] | null = null;
-    private _phrase: {[key: string]: number}[] | null = null;
-
-    constructor(phrase: {[key: string]: number}[] | null) { 
-        if (phrase) { 
-            this._phrase = phrase;
-            this.createNotes();
-        }
-    }
-
-    createNotes() {
-        for (let note of this._phrase) { 
-            this._notes.push(new Note(note));
-        }
-    }
-}
-
-// note class 
-class Note {
-    private _note: {[key: string]: number} | null = null;
-
-    constructor(note: {[key: string]: number} | null){ 
-        if (note) {
-            this._note = note;
-        }
-    }
-}
+// create level, phrases and notes with assumed input 
+let levelInput: {[key: string]: number}[][] = [
+    [{'duration': 0.5, 'pitch': 0}, {'duration': 1, 'pitch': 1}, {'duration': 1, 'pitch': 0}],
+    [{'duration': 1, 'pitch': 1}, {'duration': 0.5, 'pitch': 0}, {'duration': 1, 'pitch': 0}]];
+let levelObject: Level = new Level(1, levelInput); 
+levelObject.drawNotes(0);
 
 /*
 // draw on canvas 
