c2fd984ef6e85a0183df4422dcceee2754bf2c6b
James Wallwork <jamesleewallwork@gmail.com>
2025-03-06 16:11:01 +0000

    test extracting note data from midi

--- server/midi/midi-testing.js
+++ server/midi/midi-testing.js
@@ -1,19 +1,30 @@
-import pkg from '@tonejs/midi';
+import pkg from "@tonejs/midi";
 const { Midi } = pkg;
 
-import fs from 'fs';
+import fs from "fs";
+import path from "path";
+const __dirname = import.meta.dirname;
 
-const midiData = fs.readFileSync("test.mid")
-const midi = new Midi(midiData)
+const midiData = fs.readFileSync(path.resolve(__dirname, "test.mid"));
+const midi = new Midi(midiData);
 
-await midi.tracks.forEach(track => {  // taken from https://www.npmjs.com/package/@tonejs/midi?activeTab=readme  
-    //notes are an array
-    const notes = track.notes
-    notes.forEach(note => {
-      //note.midi, note.time, note.duration, note.name
-    console.log(note.time, '\t', note.duration, '\t', note.pitch)
-    })
-  
-    //the track also has a channel and instrument
-    //track.instrument.name
-  })
+for (const { notes } of midi.tracks) {
+    console.log([
+        ...notes.map((note) => [note.time, note.duration, note.pitch]),
+    ]);
+}
+
+// midi.tracks.forEach((track) => {
+//     //notes are an array
+//     const notes = track.notes;
+//     notes.forEach((note) => {
+//         //note.midi, note.time, note.duration, note.name
+//         console.log(note.time, "\t", note.duration, "\t", note.pitch);
+//     });
+//     return { time: note.time };
+//
+//     //the track also has a channel and instrument
+//     //track.instrument.name
+// });
+//
+
