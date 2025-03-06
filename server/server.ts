cf0840fd447bfc37c3bc55c0b4aaf09f2ccdb9ea
James Wallwork <jamesleewallwork@gmail.com>
2025-03-06 11:25:35 +0000

    fix static file server

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.use(express.static("client"));
-app.use(express.static("client/pages"));
+app.use(express.static("../client", { index: "pages/index.html" }));
 
-console.log("http://localhost:8080");
-
-app.listen(8080);
+app.listen(8080, () => console.log("http://localhost:8080"));
