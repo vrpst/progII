9a46536b1eb9a7375f2103dde2334fa5355df95c
James Wallwork <jamesleewallwork@gmail.com>
2025-03-06 11:37:41 +0000

    serve pages so relative paths work

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.use(express.static("../client", { index: "pages/index.html" }));
+app.use(express.static("../client"));
+app.use(express.static("../client/pages/"));
 
 app.listen(8080, () => console.log("http://localhost:8080"));
