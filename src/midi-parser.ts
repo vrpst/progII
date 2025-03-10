dfd9b412e7fefc5dc79250107807dd45b7a8cff9
James Wallwork <jamesleewallwork@gmail.com>
2025-03-10 16:30:34 +0000

    create interface for useful data

--- src/midi-parser.ts
+++ src/midi-parser.ts
@@ -1,9 +1,15 @@
 import { Midi } from "@tonejs/midi";
 import { Note } from "@tonejs/midi/dist/Note";
 
+interface MidiData {
+    pitch: string;
+    duration: number;
+    time: number;
+}
+
 Midi.fromUrl("test.mid").then((midi) => {
     const notes_array: Note[][] = midi.tracks.map((track) => track.notes);
-    const data = notes_array.map((notes) =>
+    const data: MidiData[][] = notes_array.map((notes) =>
         notes.map(({ pitch, duration, time }) => {
             return { pitch, duration, time };
         }),
