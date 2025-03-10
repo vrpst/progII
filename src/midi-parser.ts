737eb0d08784d84b3396c9defbeba050e64ea286
James Wallwork <jamesleewallwork@gmail.com>
2025-03-10 16:26:39 +0000

    extract useful data

--- src/midi-parser.ts
+++ src/midi-parser.ts
@@ -1,3 +1,12 @@
 import { Midi } from "@tonejs/midi";
+import { Note } from "@tonejs/midi/dist/Note";
 
-Midi.fromUrl("test.mid").then((midi) => console.log(midi));
+Midi.fromUrl("test.mid").then((midi) => {
+    const notes_array: Note[][] = midi.tracks.map((track) => track.notes);
+    const data = notes_array.map((notes) =>
+        notes.map(({ pitch, duration, time }) => {
+            return { pitch, duration, time };
+        }),
+    );
+    console.log(data);
+});
