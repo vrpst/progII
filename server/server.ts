56a8481892ecb039118c2e6599e1b492ed858695
James Wallwork <jamesleewallwork@gmail.com>
2025-03-10 14:46:25 +0000

    Express type

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { Express } from "express";
 
-const app = express();
+const app: Express = express();
 
 app.use(express.static("../client"));
 app.use(express.static("../client/pages/"));
