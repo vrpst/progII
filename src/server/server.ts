584711048d9f6e6ae52b2e47848e95211e5905ce
James Wallwork <jamesleewallwork@gmail.com>
2025-03-13 16:30:05 +0000

    fix server path

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
+import path from "path";
+const __dirname = import.meta.dirname;
 
 const app: Express = express();
 
-app.use(express.static("../client"));
-app.use(express.static("../client/pages/"));
+app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, "../client/pages/")));
 
 app.listen(8080, () => console.log("http://localhost:8080"));
