4b37458d63acbeee4a6012ad4329c014bdb7284d
vrpst <lucas.verpoest@gmail.com>
2025-04-06 19:32:24 +0100

    set up web-audio branch and initial API for serving files

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,3 +8,13 @@ app.use(express.static(path.join(__dirname, "client")));
 app.use(express.static(path.join(__dirname, "client/pages/")));
 
 app.listen(8080, () => console.log("http://localhost:8080"));
+
+app.get('/api/audio/', function(req, resp){ // serve the midi file
+    console.log(req.query)
+    const level: number = Number(req.query.level) 
+    const phrase: number = Number(req.query.phrase)
+    let audio_path: string = ""
+    audio_path = audio_path.concat('./audio/', level.toString(), '/', phrase.toString(), '.mid')
+    console.log(audio_path)
+    resp.sendFile(path.join(__dirname, audio_path))
+})
