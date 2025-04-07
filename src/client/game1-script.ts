8e6d2bc362d6bc00d9302efab57bc5bb988e3cbc
Thomas Blair <tomblair3008@yahoo.com>
2025-04-07 10:16:43 +0100

    note drawing matches scoring, needs to be styled better :( still need to code the phase where the user makes inputs and link that to the scoring function

--- src/client/game1-script.ts
+++ src/client/game1-script.ts
@@ -48,10 +48,10 @@ class Phrase {
     }
 
     handleInput(inputTime: number) { //T Takes a keypress during the players turn and gives points and scores notes
-        let minTimeDiff: number = Infinity;
+        let minTimeDiff: number = Infinity; // this and input time are in milliseconds
         let closestNote: Note | null = null;
         for(let note of this._notes) { // Finds the closest note to the press and the time difference
-            let timeDiff: number = Math.abs(note.getXPos() - inputTime);
+            let timeDiff: number = Math.abs(note.getXPos()*1000 - inputTime); // *1000 to get ms from s
             if (timeDiff < minTimeDiff) {
                 minTimeDiff = timeDiff;
                 closestNote = note;
@@ -62,15 +62,15 @@ class Phrase {
             this._badInputCount ++;
         } else {
             this._score += pointsToAdd; // add the points
-            closestNote.score(); // score the note
+            closestNote.score(minTimeDiff); // score the note
         }
     }
 
     pointsFromDiff(timeDiff: number) { //T takes a time difference and returns points
-        if (timeDiff <= 0.03) return 100;  // ≤30ms
-        if (timeDiff <= 0.08) return 80;   // ≤80ms
-        if (timeDiff <= 0.14) return 50;   // ≤140ms
-        if (timeDiff <= 0.2) return 20;    // ≤200ms
+        if (timeDiff <= 30) return 100;  // ≤30ms
+        if (timeDiff <= 80) return 80;   // ≤80ms
+        if (timeDiff <= 140) return 50;   // ≤140ms
+        if (timeDiff <= 200) return 20;    // ≤200ms
         return -40; // too far out
     }
     async drawNotes() {
@@ -91,6 +91,9 @@ class Phrase {
                 }
             }
         }
+        if (ctx) { //T Resets the canvas colour back to --red
+            ctx.strokeStyle, ctx.fillStyle = red;
+        }
         this._currentFrame += 1;
     }
 }
@@ -102,6 +105,13 @@ class Note {
     private _xPos: number | null = null; 
     private _duration: number | null = null;
     private _scored: boolean = false; //T false if the user hasn't tried to hit the note, true otherwise
+    private _accuracy: number = Math.floor(Math.random() * 5 - 1); /* T accuracy of user hit for a note
+    -1 -> not scored yet
+    0 -> perfect
+    1 -> great
+    2 -> okay
+    3 -> bad
+    */
 
     constructor(xPos: number, duration: number){ 
         this._xPos = xPos; 
@@ -116,19 +126,55 @@ class Note {
         return this._duration
     }
 
+    getAccuracy() { //T Gets the accuracy from when the note was scored
+        return this._accuracy
+    }
     isScored() { //T tells you if a boolean is scored
         return this._scored
     }
 
-    score() { //T Makes _scored true
-        this._scored = true
+    score(timeDiff: number) { //T Makes _scored true and updates accuracy
+        this._scored = true;
+        if (timeDiff <= 30) this._accuracy = 0;  // perfect
+        if (timeDiff <= 80) this._accuracy = 1;   // great
+        if (timeDiff <= 140) this._accuracy = 2;   // okay
+        if (timeDiff <= 200) this._accuracy = 3;    // bad
     }
-
     draw() { 
         if (ctx) {
             ctx.beginPath();
-            if (this._xPos != null) {
-                ctx.arc(remToPixels(this._xPos * this._spacingMult + this._startSpacing), canvas.height/2, remToPixels(0.5), 0, 2*Math.PI);
+            let accuracyText: string = ""
+            if (this._xPos != null) { 
+                console.log(this._accuracy)
+                switch (this._accuracy) { //T Changes canvas colour to match the note accuracy
+                    case 0: 
+                        ctx.strokeStyle = perfect; 
+                        ctx.fillStyle = perfect; 
+                        accuracyText = "Perfect";
+                        break;
+                    case 1: 
+                        ctx.strokeStyle = great; 
+                        ctx.fillStyle = great; 
+                        accuracyText = "Great";
+                        break;
+                    case 2: 
+                        ctx.strokeStyle = okay; 
+                        ctx.fillStyle = okay; 
+                        accuracyText = "Okay";
+                        break;
+                    case 3:
+                        ctx.strokeStyle = bad; 
+                        ctx.fillStyle = bad;
+                        accuracyText = "Bad";
+                        break;
+                    default:
+                        ctx.strokeStyle = red; 
+                        ctx.fillStyle = red;
+                }
+                let x = remToPixels(this._xPos * this._spacingMult + this._startSpacing);
+                let y = canvas.height/2;
+                ctx.arc(x, y, remToPixels(0.5), 0, 2*Math.PI);
+                ctx.fillText(accuracyText, x, y - remToPixels(1));
             }
             ctx.stroke();
         } 
@@ -138,8 +184,16 @@ class Note {
 // get css root variables 
 let rootStyle = window.getComputedStyle(document.body) as CSSStyleDeclaration | null; 
 let red: string | null = null;
+let perfect: string | null = null;
+let great: string | null = null;
+let okay: string | null = null;
+let bad: string | null = null;
 if (rootStyle) { 
     red = rootStyle.getPropertyValue('--red');
+    perfect = rootStyle.getPropertyValue('--perfect');
+    great = rootStyle.getPropertyValue('--great');
+    okay = rootStyle.getPropertyValue('--okay');
+    bad = rootStyle.getPropertyValue('--bad');
 } 
 
 // create canvas
@@ -164,6 +218,7 @@ let gameInterval: NodeJS.Timeout;
 if (canvas.getContext) {
     ctx = canvas.getContext('2d');
     if (ctx) {
+        ctx.font = `${remToPixels(1.5)}px sans-serif`; // adjust font size if needed
         if (red) {
             ctx.strokeStyle = red;
         }
