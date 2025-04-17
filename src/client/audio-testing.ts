8ea995f084cd389f32abcee87b04b0486e546e24
vrpst <lucas.verpoest@gmail.com>
2025-04-17 14:26:02 +0100

    make audio play and remove unneeded api endpoints

--- src/client/audio-testing.ts
+++ src/client/audio-testing.ts
@@ -1,6 +1,6 @@
 // start audio
 const sound = new Audio('/assets/audio/1/1.wav')
-document.addEventListener("mousemove", function () {
+document.addEventListener("keypress", function () {
     sound.muted = false
     console.log("DONE")  //  figure out what below is
     //Uncaught (in promise) NotAllowedError: play() failed because the user didn't interact with the document first. https://goo.gl/xX8pDD
--- src/server/server.ts
+++ src/server/server.ts
@@ -11,24 +11,3 @@ app.use(express.static(path.join(__dirname, "client/pages/")));
 
 
 app.listen(8080, () => console.log("http://localhost:8080"));
-
-app.get('/api/sound', function(req, resp){ // serve the midi file
-    console.log(req.query)
-    const level: string = String(req.query.level) 
-    const phrase: string = String(req.query.phrase)
-    console.log(level, phrase)
-    let sound_path: string = ""
-    sound_path = sound_path.concat('./audio/', level, '/', phrase, '.wav')
-    console.log(sound_path)
-    resp.sendFile(path.join(__dirname, sound_path))
-})
-
-app.get('/api/midi', function(req, resp){ // serve the midi file
-    console.log(req.query)
-    const level: number = Number(req.query.level) 
-    const phrase: number = Number(req.query.phrase)
-    let midi_path: string = ""
-    midi_path = midi_path.concat('./audio/', level.toString(), '/', phrase.toString(), '.mid')
-    console.log(midi_path)
-    resp.sendFile(path.join(__dirname, midi_path))
-})
