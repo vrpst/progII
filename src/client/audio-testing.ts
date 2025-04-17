e999578d2a4fe156cbdf239596356eadb48463e5
vrpst <lucas.verpoest@gmail.com>
2025-04-17 14:41:07 +0100

    general cleanup from audio merge

--- src/client/audio-testing.ts
+++ src/client/audio-testing.ts
@@ -2,7 +2,6 @@
 const sound = new Audio('/assets/audio/1/1.wav')
 document.addEventListener("keypress", function () {
     sound.muted = false
-    console.log("DONE")  //  figure out what below is
-    //Uncaught (in promise) NotAllowedError: play() failed because the user didn't interact with the document first. https://goo.gl/xX8pDD
+    console.log("DONE")
     sound.play()
 })
