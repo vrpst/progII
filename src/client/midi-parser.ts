33f65d3a0ebc2419815319d3e7c0cdc0a638971e
vrpst <lucas.verpoest@gmail.com>
2025-04-26 17:10:29 +0100

    update midi parser to create list of phrases

--- src/client/midi-parser.ts
+++ src/client/midi-parser.ts
@@ -7,7 +7,27 @@ interface MidiData {
     time: number;
 }
 
-export async function parse_midi(url: string): Promise<MidiData[][]> {
+function build_midi(levelNumber: number): Array<MidiData[][]> {
+    const levelMidi: Array<MidiData[][]> = []
+    let midiFound: boolean = true
+    let phraseNumber: number = 0
+    while (midiFound) {
+        phraseNumber += 1
+        const midiUrl = '../assets/audio/' + levelNumber.toString() + '/' + phraseNumber.toString() + '.mid'
+        try {
+            parse_midi(midiUrl).then((data) => {
+                levelMidi.push(data); 
+            })
+        }
+        catch (error) {
+            console.log(error)
+            midiFound = false
+        }
+    }
+    return levelMidi
+}
+
+async function parse_midi(url: string): Promise<MidiData[][]> {
     const midi = await Midi.fromUrl(url);
 
     const notes_array: Note[][] = midi.tracks.map((track) => track.notes);
